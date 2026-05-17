fca11d6cefccb5fc502a7b66a8e328370e6dbd0d
Eltavine <me@eltavine.com>
2026-05-17 21:36:42 +0800

    update keyword
    Corrected missing comma in the list of keywords.

--- app/src/main/java/com/eltavine/duckdetector/features/kernelcheck/data/repository/KernelCheckRepository.kt
+++ app/src/main/java/com/eltavine/duckdetector/features/kernelcheck/data/repository/KernelCheckRepository.kt
@@ -841,7 +841,7 @@ class KernelCheckRepository(
             "xiaoxiaow", "qdykernel", "numbers", "cctv",
             "arter97", "blu_spark", "elementalx", "franco", 
             "kirisakura", "sultan", "nethunter", "kdrag0n",
-            "example", "mishka", "lyrico" "hyperhelper", "niko",
+            "example", "mishka", "lyrico" "hyperhelper",
             "aptkernel", "coolzyd9107", "aptusitu", "Glow-v"
         ).map { keyword ->
             KernelKeywordRule(keyword = keyword, ignoreCase = true)
